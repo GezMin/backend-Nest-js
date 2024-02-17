a16642727c6d1101e394c2db1fa64b540217f3b8
GezMin <s.shvetsov.v@gmail.com>
2024-02-17 23:42:36 +0600

    added class-transformer class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsString, Length } from 'class-validator'
 
 export class CreateUserDto {
     @ApiProperty({ example: 'email@gmail.com', description: 'Email' })
+    @IsString({ message: 'Must be a string' })
+    @IsEmail({}, { message: 'Incorrect email' })
     readonly email: string
 
     @ApiProperty({ example: '12345678', description: 'Password' })
+    @IsString({ message: 'Must be a string' })
+    @Length(4, 16, { message: 'Must be more than 4 and less than 16' })
     readonly password: string
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    UseGuards,
+    UsePipes,
+} from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { User } from './users.model'
-
 import { Roles } from 'src/auth/roles-auth.decorator'
 import { RolesGuard } from 'src/auth/roles.guard'
 import { addRoleDto } from './dto/add-role.dto'
